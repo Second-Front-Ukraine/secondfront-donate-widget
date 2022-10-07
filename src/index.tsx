cefa8a8ad2d2f9381448033aa77b28c2d687e65b
onebit0fme <yanchynskyy@pm.me>
2022-10-07 10:20:10 -0400

    WordPress should not exist

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,16 @@ if (campaign_anchor) {
   }
 }
 
+// I'm sick of tired of WordPress and will change what I want with Javascript here. 
+const twitter_menu_top = document.getElementById("menu-item-2576");
+if (twitter_menu_top) {
+  twitter_menu_top.classList.add("social-icon-nav-item");
+}
+const twitter_menu_bottom = document.getElementById("menu-item-2579");
+if (twitter_menu_bottom) {
+  twitter_menu_bottom.classList.add("social-icon-nav-item-footer");
+}
+
 const postHeader = document.querySelector("header#header.header-blog") as HTMLElement
 if (postHeader) {
   postHeader.style.backgroundImage = "url(http://secondfrontukraine.com/wp-content/uploads/2022/10/20220529UkrainiansAtOttawaRW_TA317-black.jpg)"
